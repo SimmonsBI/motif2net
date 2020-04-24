284bbd6771e4050ea04bda0c62ce1457701d56ae
SimmonsBI <benno.simmons@gmail.com>
2020-04-24 14:32:22 +0100

    Allow integer variables in argument checks

--- R/motif2net.R
+++ R/motif2net.R
@@ -1,7 +1,7 @@
 #' @export
 motif2net <- function(target_motif_distribution, distribution_type, rows = NULL, columns = NULL, connectance = NULL, steps = 1000, iterations_at_each_temp = 1, alpha = 0.99){
   # argument checks
-  if(!(inherits(target_motif_distribution, "numeric") | inherits(target_motif_distribution, "integer"))){stop("'target_motif_distribution' must be a numeric vector containing raw or normalised motif frequencies")}
+  if(!(inherits(target_motif_distribution, "numeric") | inherits(target_motif_distribution, "integer"))){stop("'target_motif_distribution' must be a numeric or integer vector containing raw or normalised motif frequencies")}
   if(!length(target_motif_distribution) %in% c(17,44)){stop("target_motif_distribution must be of length 17 (if only considering up to five node motifs) or 44 (if considering up to six node motifs)")}
   six_node <- ifelse(length(target_motif_distribution) == 44, yes = TRUE, no = FALSE)
   if(length(target_motif_distribution) == 17 & six_node == TRUE){stop("target_motif_distribution must be of length 44 if six_node = TRUE because there are 44 motifs up to six nodes. Either change the length of target_motif_distribution or set six_node = FALSE")}
@@ -12,15 +12,15 @@ motif2net <- function(target_motif_distribution, distribution_type, rows = NULL,
   if(distribution_type == "normalise_sizeclass" & sizeclass_check(target_motif_distribution) == FALSE){stop("When distribution_type = 'normalise_sizeclass' elements of target_motif_distribution must sum to 1 within each motif size class")}
   if(distribution_type == "normalise_levelsize" & levelsize_check(target_motif_distribution) == FALSE){stop("When distribution_type = 'normalise_levelsize' elements of target_motif_distribution must sum to 1 within each motif levelsize class (a group of motifs with a given number of nodes in the top level and the bottom level)")}
   if(!is.null(rows)){
-    if(!inherits(rows, "numeric")){stop("'rows' must be numeric")}
+    if(!inherits(rows, c("numeric","integer"))){stop("'rows' must be numeric or integer")}
   }
   if(!is.null(columns)){
-    if(!inherits(columns, "numeric")){stop("'columns' must be numeric")}
+    if(!inherits(columns, c("numeric","integer"))){stop("'columns' must be numeric or integer")}
   }
   if(!is.null(connectance)){
     if(!inherits(connectance, "numeric")){stop("'connectance' must be numeric")}
   }
-  if(!inherits(steps, "numeric")){stop("'steps' must be numeric")}
+  if(!inherits(steps, c("numeric","integer"))){stop("'steps' must be numeric or integer")}
   if(!inherits(iterations_at_each_temp, "numeric")){stop("'iterations_at_each_temp' must be numeric")}
   if(!inherits(alpha, "numeric")){stop("'alpha' must be numeric")}
   # run analysis
